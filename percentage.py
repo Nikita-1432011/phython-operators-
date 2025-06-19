d8f9870d1a6cc6f399329ce7daab3d6a09c41142
Nikita-1432011 <steppers.nikita3159@gmail.com>
2025-06-19 20:51:43 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,11 @@
+a=int(input("Enter raj's Maths Marks:")) 
+b=int(input("Enter raj's Science Marks:"))
+c=int(input("Enter raj's Hindi Marks:"))
+d=int(input("Enter raj's English Marks:"))
+
+
+total= a+b+c+d
+percentage= (total/400)*100
+
+print("Total Marks of Rj =", percentage)
+
